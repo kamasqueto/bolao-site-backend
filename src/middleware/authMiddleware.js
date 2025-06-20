a9dbc4f4231df2c6133dbf8b9d7449869787ed90
katmasqueto <kaiquemasqueto@gmail.com>
2025-06-19 21:30:02 -0300

    debug

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,6 +2,7 @@
 import jwt from 'jsonwebtoken';
 
 export function verificarToken(req, res, next) {
+  console.log("Authorization Header:", req.headers.authorization); // debug
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) return res.status(401).json({ error: 'Token não fornecido' });
 
