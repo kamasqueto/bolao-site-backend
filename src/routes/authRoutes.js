9eb3012f305f231f2ce6c1586f65df3575b9fc91
katmasqueto <kaiquemasqueto@gmail.com>
2025-06-21 08:55:39 -0300

    Correção Rota Auth

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { verificarToken } from "../middleware/authMiddleware";
+import { verificarToken } from "../middleware/authMiddleware.js";
 
 // chave secreta (use algo melhor e guarde no .env em produção)
 const JWT_SECRET = process.env.JWT_SECRET;
